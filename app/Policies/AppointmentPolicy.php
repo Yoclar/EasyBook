45fcf7c1c42d450b7edd2afef51f54f1499071c3
Ricsi <thereallemonmail@gmail.com>
2025-03-11 15:11:19 +0100

    a few error fixes, workflow check

--- app/Policies/AppointmentPolicy.php
+++ app/Policies/AppointmentPolicy.php
@@ -6,7 +6,7 @@ use App\Models\Appointments;
 use App\Models\User;
 use Illuminate\Auth\Access\Response;
 
-class AppointmentsPolicy
+class AppointmentPolicy
 {
     /**
      * Determine whether the user can view any models.
@@ -19,7 +19,7 @@ class AppointmentsPolicy
     /**
      * Determine whether the user can view the model.
      */
-    public function view(User $user, Appointments $appointments): bool
+    public function view(User $user, Appointment $appointments): bool
     {
         return false;
     }
@@ -35,7 +35,7 @@ class AppointmentsPolicy
     /**
      * Determine whether the user can update the model.
      */
-    public function update(User $user, Appointments $appointments): bool
+    public function update(User $user, Appointment $appointments): bool
     {
         return false;
     }
@@ -43,7 +43,7 @@ class AppointmentsPolicy
     /**
      * Determine whether the user can delete the model.
      */
-    public function delete(User $user, Appointments $appointments): bool
+    public function delete(User $user, Appointment $appointments): bool
     {
         return false;
     }
@@ -51,7 +51,7 @@ class AppointmentsPolicy
     /**
      * Determine whether the user can restore the model.
      */
-    public function restore(User $user, Appointments $appointments): bool
+    public function restore(User $user, Appointment $appointments): bool
     {
         return false;
     }
@@ -59,7 +59,7 @@ class AppointmentsPolicy
     /**
      * Determine whether the user can permanently delete the model.
      */
-    public function forceDelete(User $user, Appointments $appointments): bool
+    public function forceDelete(User $user, Appointment $appointments): bool
     {
         return false;
     }
--- app/Policies/ProviderProfilePolicy.php
+++ app/Policies/ProviderProfilePolicy.php
@@ -2,11 +2,11 @@
 
 namespace App\Policies;
 
-use App\Models\Provider_Profiles;
+use App\Models\ProviderProfile;
 use App\Models\User;
 use Illuminate\Auth\Access\Response;
 
-class ProviderProfilesPolicy
+class ProviderProfilePolicy
 {
     /**
      * Determine whether the user can view any models.
@@ -19,7 +19,7 @@ class ProviderProfilesPolicy
     /**
      * Determine whether the user can view the model.
      */
-    public function view(User $user, Provider_Profiles $providerProfiles): bool
+    public function view(User $user, ProviderProfile $providerProfiles): bool
     {
         return false;
     }
@@ -35,7 +35,7 @@ class ProviderProfilesPolicy
     /**
      * Determine whether the user can update the model.
      */
-    public function update(User $user, Provider_Profiles $providerProfiles): bool
+    public function update(User $user, ProviderProfile $providerProfiles): bool
     {
         return false;
     }
@@ -43,7 +43,7 @@ class ProviderProfilesPolicy
     /**
      * Determine whether the user can delete the model.
      */
-    public function delete(User $user, Provider_Profiles $providerProfiles): bool
+    public function delete(User $user, ProviderProfile $providerProfiles): bool
     {
         return false;
     }
@@ -51,7 +51,7 @@ class ProviderProfilesPolicy
     /**
      * Determine whether the user can restore the model.
      */
-    public function restore(User $user, Provider_Profiles $providerProfiles): bool
+    public function restore(User $user, ProviderProfile $providerProfiles): bool
     {
         return false;
     }
@@ -59,7 +59,7 @@ class ProviderProfilesPolicy
     /**
      * Determine whether the user can permanently delete the model.
      */
-    public function forceDelete(User $user, Provider_Profiles $providerProfiles): bool
+    public function forceDelete(User $user, ProviderProfile $providerProfiles): bool
     {
         return false;
     }
